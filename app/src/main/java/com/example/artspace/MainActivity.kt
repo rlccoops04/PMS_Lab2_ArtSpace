fbb7152030274eb10b19c12e290a6369d6736374
Bulat <@LAPTOP-SSS2TI2M>
2023-10-29 17:34:21 +0300

    Init project

---
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtSpaceTheme {
+                ArtSpace_app()
+            }
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun ArtSpace_app(modifier: Modifier = Modifier) {
+    ArtSpace_ImageAndButtons()
+}
+
+@Composable
+fun ArtSpace_ImageAndButtons() {
+
+}
